af63f1bfb5bb0fb4327aaed8e9a83851be60646b
Volodymyr.Glushak <volodymyr.glushak@valtech.co.uk>
2017-06-03 16:08:50 +0100

    Extended example.

--- src/main/scala/io/github/hbase4s/examples/HelloHBase.scala
+++ src/main/scala/io/github/hbase4s/examples/HelloHBase.scala
@@ -2,6 +2,7 @@ package io.github.hbase4s.examples
 
 import io.github.hbase4s.config.HBasePropsConfig
 import io.github.hbase4s.{HBaseClient, HBaseConnection}
+import io.github.hbase4s.filter._
 
 /**
   * Example 1
@@ -15,10 +16,37 @@ import io.github.hbase4s.{HBaseClient, HBaseConnection}
 object HelloHBase extends App {
 
   val props = Map("hbase.zookeeper.quorum" -> "localhost")
-  val client: HBaseClient = new HBaseClient(new HBaseConnection(new HBasePropsConfig(props)), "transactions")
+  val transactions: HBaseClient = new HBaseClient(new HBaseConnection(new HBasePropsConfig(props)), "transactions")
 
-  case class Event(id: Int, desc: String, enabled: Boolean)
+  case class Event(uniqueId: Long, amount: Double, buyer: String, payer: String, date: String, completed: Boolean)
 
-  client.put("id_12", Event(15, "some", true))
 
+  private val ev = Event(15L, 150000d, "goldman", "china", "2018/01/01", true)
+
+  println("Insert record to HBase")
+  transactions.put(ev.uniqueId, ev)
+
+  println("Retrieving record from HBase by Id")
+  val storedEvent = transactions.get(15L).map(_.typed[Event].asClass)
+
+  require(Some(ev) == storedEvent)
+
+  println("Query all transactions for particular buyer with amount greater than 100k")
+  val foundTransactions = transactions.scan[Long](
+    "(event:buyer == goldman) AND (event:amount > double(100000))"
+  ).map(_.typed[Event].asClass).toList
+
+  require(foundTransactions.size == 1)
+  require(foundTransactions == List(ev))
+
+  println("Same filter with different static DSL")
+  val foundTransactions2 = transactions.scan[Long](
+    c("event", "buyer") === "goldman" & c("event", "amount") > 100000.0
+  ).map(_.typed[Event].asClass).toList
+
+  require(foundTransactions2.size == 1)
+  require(foundTransactions2 == List(ev))
+
+  println("Drop our transaction from HBase")
+  transactions.delete(15L)
 }
